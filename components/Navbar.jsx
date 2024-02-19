53f93f66b996c5a651b199885ed63fd67cb1b45a
Pooja Gurung <pooja_grg07@hotmail.com>
2024-02-19 17:30:34 +0000

    navbar added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,9 @@
 export default function Navbar() {
   return (
-    <nav>
-      travel journel
+    <nav className="nav--section">
+      <img src="/public/boarding-pass.png" className="navbar--logo" alt="boarding-pass" />
+      <h2>Travel Journel</h2>
+      <img src="/public/travel.png" className="navbar--logo" alt="travel" />
     </nav>
   )
 }
