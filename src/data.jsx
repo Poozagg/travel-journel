9c54a4d4677bb15b0e5bd4a4e9adb8725fd0c7ff
Pooja Gurung <pooja_grg07@hotmail.com>
2024-02-20 16:33:40 +0000

    prop-types library installed. card component with prop created.

--- src/data.jsx
+++ src/data.jsx
@@ -2,31 +2,31 @@ export default [
   {
       id: 1,
       title: "Mount Fuji",
-      location: "Japan",
+      location: "JAPAN",
       googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
       startDate: "12 Jan, 2017",
       endDate: "24 Jan, 2017",
       description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-      image: "japan.png"
+      img: "japan.png"
   },
   {
     id: 2,
     title: "Mega Adventure - Singapore",
-    location: "Singapore",
+    location: "SINGAPORE",
     googleMapsUrl: "https://www.google.com/maps/place/Singapore/@1.3565285,103.7266195,11.36z/data=!4m6!3m5!1s0x31da11238a8b9375:0x887869cf52abf5c4!8m2!3d1.352083!4d103.819836!16zL20vMDZ0MnQ?entry=ttu",
     startDate: "12 Apr, 2018",
     endDate: "24 Apr, 2018",
     description: "Mega Adventure in Sentosa, Singapore, offers an electrifying ziplining experience with breathtaking views of lush landscapes and sparkling waters. Suspended high above the island, the zipline provides an adrenaline rush and a unique perspective of Sentosa's beauty, making it a must-try attraction for thrill-seekers and nature enthusiasts alike.",
-    image: "singapore.png"
+    img: "singapore.png"
   },
   {
     id: 3,
     title: "Wat Arun",
-    location: "Thailand",
+    location: "THAILAND",
     googleMapsUrl: "https://www.google.com/maps/place/Thailand/@12.8211851,90.8230779,5z/data=!3m1!4b1!4m6!3m5!1s0x304d8df747424db1:0x9ed72c880757e802!8m2!3d15.870032!4d100.992541!16zL20vMDdmMXg?entry=ttu",
     startDate: "12 Nov, 2019",
     endDate: "24 Nov, 2019",
     description: "Wat Arun, situated on the banks of the Chao Phraya River in Bangkok, Thailand, is a captivating temple known for its distinctive spires that gracefully pierce the skyline. Adorned with intricate ceramic mosaics and steeped in rich history, Wat Arun, also known as the Temple of Dawn, is a cultural gem. As the sun sets, the temple illuminates, casting a mesmerizing reflection on the river, creating a magical ambiance for visitors to marvel at Thailand's architectural and spiritual wonders.",
-    imageUrl: "thailand.png"
+    img: "thailand.png"
   }
 ]
