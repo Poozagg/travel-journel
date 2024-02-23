aa7255229610a2fc5f5e73b2524ecf32c4f1f96b
Pooja Gurung <pooja_grg07@hotmail.com>
2024-02-23 11:45:13 +0000

    journal spelling corrected

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <nav className="nav--section">
       <img src="/images/boarding-pass.png" className="navbar--logo" alt="boarding-pass" />
-      <h2>Travel Journel</h2>
+      <h2>Travel Journal</h2>
       <img src="/images/travel.png" className="navbar--logo" alt="travel" />
     </nav>
   )
